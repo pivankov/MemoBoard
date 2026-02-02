2336d21d07d66bb4be49652b8e912bbcb86c3599
Ilya Pivankov <18445560+pivankov@users.noreply.github.com>
2026-02-02 12:05:11 +0300

    Adding bookmarks system categories IDs to const

--- frontend/src/constants/bookmarks.ts
+++ frontend/src/constants/bookmarks.ts
@@ -6,6 +6,10 @@
  * ID системных категорий
  */
 export const SYSTEM_CATEGORIES = {
+  /** ID коллекции "Системные" */
+  SYSTEM: '0000',
+  /** ID категории "Несортированные" */
+  UNSORTED: 'ktGDhX',  
   /** ID категории "Корзина" */
   TRASH: 'wxiqnC',
 } as const;
