4b06f221fee295d69a6b67e622899acf29bcaa71
Ilya Pivankov <18445560+pivankov@users.noreply.github.com>
2026-03-25 11:34:33 +0300

    Updating BookmarksSidebarList, deleting unused SYSTEM_CATEGORIES, updating isDropdownAvailable

--- frontend/src/components/Bookmarks/BookmarksSidebarList.tsx
+++ frontend/src/components/Bookmarks/BookmarksSidebarList.tsx
@@ -1,7 +1,6 @@
 import { BookmarksSidebarListType } from "types/bookmarks";
 
 import BookmarksSidebarDropdown from "./BookmarksSidebarDropdown";
-import { SYSTEM_CATEGORIES } from 'constants/bookmarks';
 
 import "./BookmarksSidebarList.css";
 
@@ -10,25 +9,28 @@ interface BookmarksSidebarListProps {
   id: string;
   title: string;
   children: React.ReactNode;
+  showDropdown?: boolean;
 };
 
-const BookmarksSidebarList: React.FC<BookmarksSidebarListProps> = ({ type, id, title, children }) => {
-  const isDropdownAvailable = id !== SYSTEM_CATEGORIES.SYSTEM;
+const BookmarksSidebarList: React.FC<BookmarksSidebarListProps> = ({ type, id, title, children, showDropdown = true }) => {
+  const isDropdownAvailable = showDropdown;  
 
   return (
     <div className="bookmarks-sidebar-list">
-      <div className="bookmarks-sidebar-list__title">
-        <div className="bookmarks-sidebar-list__title-label">
-          {title}
+      {title && (
+        <div className="bookmarks-sidebar-list__title">
+          <div className="bookmarks-sidebar-list__title-label">
+            {title}
+          </div>
+          { isDropdownAvailable && (
+            <BookmarksSidebarDropdown
+              type={type}
+              id={id}
+              title={title}
+            />
+          ) }
         </div>
-        { isDropdownAvailable && (
-          <BookmarksSidebarDropdown
-            type={type}
-            id={id}
-            title={title}
-          />
-        ) }
-      </div>
+      )}
       <div className="bookmarks-sidebar-list__wrapper">
         {children}
       </div>
