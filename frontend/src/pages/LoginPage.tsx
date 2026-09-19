9b88c31975caac972e3eab57d52c8aa8acdb8e66
Ilya Pivankov <18445560+pivankov@users.noreply.github.com>
2026-09-19 14:14:11 +0300

    Updating html/css on login and register pages

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -9,6 +9,7 @@
 import { useState } from 'react';
 import { Link, Navigate, useNavigate } from 'react-router';
 import { Button, Card, Form, Input, Typography } from 'antd';
+import { WarningFilled } from '@ant-design/icons';
 
 import { useAuth } from 'hooks/useAuth';
 
@@ -82,45 +83,15 @@ function LoginPage() {
         </Card>
       </div>
       <div className="login-page__right">
-        <h2>Welcome to our community</h2>
+        <div className="login-page__text">
+        <h1>MemoBoard</h1>
+        <p>Добро пожаловать в MemoBoard — это проект для управления событиями и закладками.</p>
+        <p>Календарь с умной группировкой дат, напоминания о праздниках и днях рождения, удобное хранение ссылок с тегами.</p>
+        <p>GitHub: <a href="https://github.com/pivankov/MemoBoard" target="blank">https://github.com/pivankov/MemoBoard</a></p>
+        <p className="login-page__text-note"><WarningFilled /> Сайт не адаптирован под мобильные устройства</p>
+        </div>         
       </div>
     </div>
-    // <div className="login-page">
-    //   <Card className="login-page__card">
-    //     <Title level={3} className="login-page__title">Вход в MemoBoard</Title>
-
-    //     {error && <Text type="danger" className="login-page__error">{error}</Text>}
-
-    //     <Form layout="vertical" onFinish={handleSubmit} autoComplete="off">
-    //       <Form.Item
-    //         label="Email"
-    //         name="email"
-    //         rules={[
-    //           { required: true, message: 'Введите email' },
-    //           { type: 'email', message: 'Некорректный формат email' },
-    //         ]}
-    //       >
-    //         <Input placeholder="demo@example.com" size="large" />
-    //       </Form.Item>
-
-    //       <Form.Item
-    //         label="Пароль"
-    //         name="password"
-    //         rules={[{ required: true, message: 'Введите пароль' }]}
-    //       >
-    //         <Input.Password placeholder="demo" size="large" />
-    //       </Form.Item>
-
-    //       <Form.Item>
-    //         <Button type="primary" htmlType="submit" loading={loading} block size="large">
-    //           Войти
-    //         </Button>
-    //       </Form.Item>
-    //     </Form>
-
-    //     <Text>Нет аккаунта? <Link to="/register">Зарегистрироваться</Link></Text>
-    //   </Card>
-    // </div>
   );
 }
 
